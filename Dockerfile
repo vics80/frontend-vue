253a9431f617623798d4e341a215155e1aafedb9
Víctor Garcia <victor.garcia@bytetravel.com>
2024-11-14 07:59:18 +0100

    project base vue

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Usar la imagen oficial de Node.js como imagen base
+FROM node:18-alpine
+
+# Establecer el directorio de trabajo dentro del contenedor
+WORKDIR /app
+
+# Copiar los archivos de dependencias al directorio de trabajo
+COPY package*.json ./
+
+# Instalar las dependencias del proyecto
+RUN npm install
+
+# Copiar el resto de los archivos de la aplicación al contenedor
+COPY . .
+
+# Exponer el puerto que usa Vite (por defecto, 5173)
+EXPOSE 5173
+
+# Comando por defecto para iniciar la aplicación
+CMD ["npm", "run", "dev"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
     },
+  },
+  define: {
+    'process.env': process.env,
   },
 })
